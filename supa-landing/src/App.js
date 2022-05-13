2e15c3b450ab440882574870761d1e9591c130ed
tombrewsviews <parandykt@gmail.com>
2022-05-13 11:37:41 +0200

    placeholder

---
+++ supa-landing/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="Logo" alt="logo" />
+      {/* <div className="Back"></div> */}
+    </div>
+  );
+}
+
+export default App;
